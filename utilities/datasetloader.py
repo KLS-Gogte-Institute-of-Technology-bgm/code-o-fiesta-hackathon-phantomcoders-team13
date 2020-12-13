8b201e59040df73b962195b01943b7142579aed2
nikitakodkany <nikitahkodkany@gmail.com>
2020-12-13 17:53:20 +0400

    Script that loads the dataset

--- utilities/datasetloader.py
+++ utilities/datasetloader.py
@@ -7,8 +7,16 @@ import tensorflow.keras as K
 
 def datasetloader():
     print()
+    batch_size = 1
+    img_height = 180
+    img_width = 180
+
     print("Loading dataset...")
+    PATH = '../'
     path = '../dataset'
+
+    # print(os.listdir(PATH))
+    dataset = os.path.join(PATH,'dataset')
 
     class1 = os.path.join(path,'class1')
     class2 = os.path.join(path,'class2')
@@ -19,6 +27,27 @@ def datasetloader():
     x3 = len(os.listdir(class3))
     checksum = x1+x2+x3
     if(checksum == 2000):
-        print("Checkpoint Passed!")
+        print("Checkpoint 1 Passed!")
+        print()
 
+    train = tf.keras.preprocessing.image_dataset_from_directory(
+        dataset,
+        validation_split = 0.1,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+
+    print()
+
+    test = tf.keras.preprocessing.image_dataset_from_directory(
+        dataset,
+        validation_split=0.1,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+
+    print()
+    return train, test, img_width, img_height
 datasetloader()
